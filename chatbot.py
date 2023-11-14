0f79b0b3c83fde8d4e7b346c7746225677575e99
robert.girard <r_girard@live.com>
2023-11-13 20:47:14 -0500

    playing around

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,12 @@
 from langchain.chat_models.base import BaseChatModel
+from git import Repo
+from langchain.memory.entity import SQLiteEntityStore
+from langchain.chains import ConversationalRetrievalChain, ConversationChain
+from langchain.chains.conversational_retrieval.base import BaseConversationalRetrievalChain
 
 class chatbot:
-    chatmodel: BaseChatModel
+    chatmodel: BaseChatModel = None
 
+    def __init__(self):
+        SQLiteEntityStore()
+        ConversationalRetrievalChain.from_llm()
